81ae70d9e50b3ac25f2225619761edb31a6f87fd
FREANCATIBOG <freanrajascatibog@gmail.com>
2024-06-26 11:45:24 +0800

    few build of dashboards and menu

---
+++ lib/pages/dashboard.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.purple[50],
+      body: Container(
+        child: SafeArea(
+          child: Center(
+            child: Column(
+              children: [
+                ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/menu');
+                  },
+                  child: Text('GONNA GO TO MENU?'),
+                ),
+                ElevatedButton(
+                  onPressed: (){
+                    Navigator.pushNamed(context, '/profile');
+                  },
+                  child: Text('GONNA GO TO PROFILE '),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
